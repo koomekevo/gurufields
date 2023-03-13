0ddaf8ea67bf18a840f5b60aca3c9814f6bedca9
koomekevo <koome.kevo@gurucrafts.net>
2023-03-13 17:23:27 +0300

    Add NodeTrait in Area.php

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
 
 class Area extends Model
 {
     use HasFactory;
+    use NodeTrait;
 }
