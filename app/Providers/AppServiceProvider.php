8f172f2461b4e7d9d5f32ea60d162be026422529
koomekevo <koome.kevo@gurucrafts.net>
2023-03-20 15:36:16 +0300

    Add creating function in AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \App\Models\Area::creating(function ($area) {
+            $prefix = $area->parent ? $area->parent->name . ' ' : '';
+            $area->slug = str_slug($prefix . $area->name);
+        });
     }
 }
